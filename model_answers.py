5aa53434e9debd5f78be7c16919d8d86549aefa6
Usha193 <164126787+Usha193@users.noreply.github.com>
2025-10-20 00:12:14 -0400

    Add files via upload

---
+++ model_answers.py
@@ -0,0 +1,13 @@
+ans={
+    "Describe the types of Data Structures?":"Data Structures are mainly classified into two types, Linear Data Structure and Non-Linear Data Structures",
+    "What is a Stack?":"Stack is an ordered list in which, insertion and deletion can be performed only at one end that is called the top.",
+    "What are the operations that can be performed on a stack?":"Push Operations,Pop Operations,Peek Operations, also called as insertion,deletion and top",
+    "What is the difference between NULL and VOID?":"Null is actually a value, whereas Void is a data type identifier.",
+    "Can you declare an interface method static? explain your answer?":"No, because methods of an interface are abstract by default, and we can not use static and abstract together.",
+    "Can the Interface be final? explain your answer?":"No, because an interface needs to be implemented by the other class and if it is final, it can't be implemented by any class.",
+    "How many types of database languages are?":"There are four types of database languages Data Definition Language (DDL).Data Manipulation Language (DML) Data Control Language (DCL) Transaction Control Language (TCL)",
+    "What are the types of entity Relationship?":"There are three type of relationships in DBMS. One-To-One, One-To-Many or many-to-one,  Many-to-many",
+    "Who is the founder of C language?":"Dennis Ritchie",
+    "what is list in python?":"List is a linear data type,which is ordered,changable and indexed",
+    "what is NLP":"NLP stands for Natural Language Processing"
+    }
---
+++ model_questions.py
@@ -0,0 +1,13 @@
+l=[
+    "Describe the types of Data Structures?",
+    "What is a Stack?",
+    "What are the operations that can be performed on a stack?",
+    "What is the difference between NULL and VOID?",
+    "Can you declare an interface method static? explain your answer?",
+    "Can the Interface be final? explain your answer?",
+    "How many types of database languages are?",
+    "What are the types of entity Relationship?",
+    "Who is the founder of C language?",
+    "what is list in python?",
+    "what is NLP"
+]
